521c984c65b7ff0500fc7f6b3e62aa941cd95076
Vamshi51282 <135943611+Vamshi51282@users.noreply.github.com>
2024-10-29 09:01:28 +0530

    Add files via upload

---
+++ 4octoberTask.js
@@ -0,0 +1,29 @@
+function addition(a, b) {
+    var a = 40
+    var b = 50
+    return a + b;
+} console.log(addition());
+
+function subract(a, b) {
+    var a = 90
+    var b = 80
+    return a - b;
+} console.log(subract());
+
+
+function multiply(a, b) {
+    var a = 50
+    var b = 60
+    return a * b;
+} console.log(multiply());
+
+
+function divide(a, b) {
+    var a = 50
+    var b = 0
+    if (b == 0) {
+        return "cant divide by zero"
+    }
+    return a / b;
+} console.log(divide());
+
